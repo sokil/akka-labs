d4480f3ff192839f605b229fb5c05491d7868b37
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-06-26 23:30:18 +0300

    implement actor system

--- src/akkalabs/App.java
+++ src/akkalabs/App.java
@@ -1,9 +1,21 @@
 package akkalabs;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+import java.io.IOException;
+
 public class App 
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        final ActorSystem system = ActorSystem.create("helloakka");
+
+        System.out.println("Press any key to continue...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+
+        }
     }
 }
