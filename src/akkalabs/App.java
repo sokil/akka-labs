ec398341fc560f3ea12b524a90ffcd5f44f518a5
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-06-26 23:40:41 +0300

    add actors

--- src/akkalabs/App.java
+++ src/akkalabs/App.java
@@ -9,13 +9,25 @@ public class App
 {
     public static void main(String[] args)
     {
-        final ActorSystem system = ActorSystem.create("helloakka");
+        // build system
+        ActorSystem system = ActorSystem.create("AkkaLabsApplication");
 
+        // add actors
+        ActorRef consumerActor = system.actorOf(ConsumerActor.props(), "consumerActor");
+
+        ActorRef producer1Actor = system.actorOf(ProducerActor.props(), "producer1Actor");
+        ActorRef producer2Actor = system.actorOf(ProducerActor.props(), "producer2Actor");
+        ActorRef producer3Actor = system.actorOf(ProducerActor.props(), "producer2Actor");
+
+
+        // terminate project
         System.out.println("Press any key to continue...");
         try {
             System.in.read();
         } catch (IOException e) {
 
+        } finally {
+            system.terminate();
         }
     }
 }
---
+++ src/akkalabs/ConsumerActor.java
@@ -0,0 +1,11 @@
+package akkalabs;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+public class ConsumerActor extends AbstractActor {
+    public Receive createReceive() {
+        return null;
+    }
+}
